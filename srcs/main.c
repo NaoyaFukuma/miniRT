c3633f434b1840cebdb6b087b8615d31eba0103a
kyamagis <103745847+kyamagis@users.noreply.github.com>
2023-01-16 12:54:31 +0900

    branch_test

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: kyamagis <kyamagis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 22:49:07 by nfukuma           #+#    #+#             */
-/*   Updated: 2023/01/16 12:51:47 by kyamagis         ###   ########.fr       */
+/*   Updated: 2023/01/16 12:54:12 by kyamagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int	main(int ac, char **av)
 
 	rt_check_arg(ac, av);
 	rt_init(&rt, av[1]);
+	
 	
 	// rt_draw(rt);
 	return (ac);
