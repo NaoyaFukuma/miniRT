3b530e9b65296a6401fa7694610096228fff02f3
nfukuma <100755561+NaoyaFukuma@users.noreply.github.com>
2023-01-16 10:10:35 +0900

    test mlx_init

--- inc/rt_strucs.h
+++ inc/rt_strucs.h
@@ -6,30 +6,49 @@
 /*   By: nfukuma <nfukuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 23:54:16 by nfukuma           #+#    #+#             */
-/*   Updated: 2023/01/14 14:56:57 by nfukuma          ###   ########.fr       */
+/*   Updated: 2023/01/15 00:21:41 by nfukuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RT_STRUCS_H
 # define RT_STRUCS_H
 
+typedef struct s_vec3
+{
+}			t_vec3;
+
+typedef struct s_fcolor
+{
+}			t_fcolor;
+
+typedef struct s_obj
+{
+}			t_obj;
+
+typedef struct s_lite
+{
+}			t_lite;
+
+typedef struct s_camera
+{
+}			t_camera;
 typedef struct s_scene
 {
-	// t_camera	camara;
-	// t_lites		*lites;
-	// t_objs		*objs;
-}				t_scene;
+	t_camera	camara;
+	t_lite		*lites;
+	t_obj		*objs;
+}			t_scene;
 
 typedef struct s_mlx
 {
-	void		*mlx;
-	void		*win;
-}				t_mlx;
+	void	*mlx;
+	void	*win;
+}			t_mlx;
 
 typedef struct s_rt_data
 {
-	t_mlx		mlx;
-	t_scene		scene;
-}				t_rt_data;
+	t_mlx	mlx;
+	t_scene	scene;
+}			t_rt_data;
 
 #endif
