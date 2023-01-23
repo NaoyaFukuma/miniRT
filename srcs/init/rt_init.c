7890c70c3b0677720acc4079a61ecc2934cffb00
nfukuma <100755561+NaoyaFukuma@users.noreply.github.com>
2023-01-24 03:38:43 +0900

    norm fill fix

--- srcs/init/rt_init.c
+++ srcs/init/rt_init.c
@@ -6,7 +6,7 @@
 /*   By: nfukuma <nfukuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 14:53:32 by nfukuma           #+#    #+#             */
-/*   Updated: 2023/01/24 03:38:06 by nfukuma          ###   ########.fr       */
+/*   Updated: 2023/01/24 03:38:40 by nfukuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,13 +53,13 @@ static void	rt_mlx_init(t_rt_data *rt)
 	if (!rt->mlx.win)
 		rt_put_error_exit("mlx_new_window() failure");
 	rt->mlx.image.img = mlx_new_image(rt->mlx.mlx, rt->mlx.image.width,
-		rt->mlx.image.height);
+			rt->mlx.image.height);
 	if (!rt->mlx.image.img)
 		rt_put_error_exit("mlx_new_window() failure");
 	rt->mlx.image.addr = mlx_get_data_addr(rt->mlx.image.img,
-							&rt->mlx.image.bits_per_pixel,
-							&rt->mlx.image.line_length,
-							&rt->mlx.image.endian);
+						&rt->mlx.image.bits_per_pixel,
+						&rt->mlx.image.line_length,
+						&rt->mlx.image.endian);
 	if (!rt->mlx.image.addr)
 		rt_put_error_exit("mlx_new_window() failure");
 }
