49115273d9ea56d7d5d42733a2b84022439e399d
nfukuma <100755561+NaoyaFukuma@users.noreply.github.com>
2023-01-23 17:26:46 +0900

    cylinder.c norm

--- srcs/draw/test_intersection/rt_cy_intersec.c
+++ srcs/draw/test_intersection/rt_cy_intersec.c
@@ -19,7 +19,7 @@
 #include <math.h>
 
 
-double	rt_cy_calculate_directional_vector_coefficients(t_cylinder *cylinder, t_ray ray, double *flag)
+double	rt_cy_calc_dir_vec_t(t_cylinder *cylinder, t_ray ray, double *flag)
 {
 	t_3d_vec d = rt_vec_copy(ray.unit_d_vec);
 	t_3d_vec s_cy_c = rt_vec_sub(ray.start, cylinder->center_p_vec);
@@ -44,8 +44,8 @@ double	rt_cy_calculate_directional_vector_coefficients(t_cylinder *cylinder, t_r
 			t = rt_min(t1, t2);
 			t_3d_vec pa = rt_get_point(ray, t);
 
-			double hDis = rt_vec_dot(rt_vec_sub(pa, cylinder->center_p_vec), cylinder->unit_orient_vec);
-			if (hDis < (- 1.0 * cylinder->height / 2.0f) || (cylinder->height / 2.0f) < hDis)
+			double h_dis = rt_vec_dot(rt_vec_sub(pa, cylinder->center_p_vec), cylinder->unit_orient_vec);
+			if (h_dis < (- 1.0 * cylinder->height / 2.0f) || (cylinder->height / 2.0f) < h_dis)
 			{
 				t = rt_max(t1, t2);
 				*flag = -1.0;
@@ -59,37 +59,51 @@ double	rt_cy_calculate_directional_vector_coefficients(t_cylinder *cylinder, t_r
 	return (t);
 }
 
+static t_3d_vec	rt_calc_normal(double flag, t_cylinder *cylinder, t_3d_vec pa)
+{
+	t_3d_vec	tmp_normal;
+
+	tmp_normal.x = 2 * flag * (cylinder->unit_orient_vec.z
+			* (cylinder->unit_orient_vec.z * (pa.x - cylinder->center_p_vec.x)
+				- cylinder->unit_orient_vec.x * (pa.z - cylinder->center_p_vec.z))
+			- cylinder->unit_orient_vec.y * (cylinder->unit_orient_vec.x * (pa.y
+					- cylinder->center_p_vec.y) - cylinder->unit_orient_vec.y * (pa.x
+					- cylinder->center_p_vec.x)));
+	tmp_normal.y = 2 * flag * (cylinder->unit_orient_vec.x
+			* (cylinder->unit_orient_vec.x * (pa.y - cylinder->center_p_vec.y)
+				- cylinder->unit_orient_vec.y * (pa.x - cylinder->center_p_vec.x))
+			- cylinder->unit_orient_vec.z * (cylinder->unit_orient_vec.y * (pa.z
+					- cylinder->center_p_vec.z) - cylinder->unit_orient_vec.z * (pa.y
+					- cylinder->center_p_vec.y)));
+	tmp_normal.z = 2 * flag * (cylinder->unit_orient_vec.y
+			* (cylinder->unit_orient_vec.y * (pa.z - cylinder->center_p_vec.z)
+				- cylinder->unit_orient_vec.z * (pa.y - cylinder->center_p_vec.y))
+			- cylinder->unit_orient_vec.x * (cylinder->unit_orient_vec.z * (pa.x
+					- cylinder->center_p_vec.x) - cylinder->unit_orient_vec.x * (pa.z
+					- cylinder->center_p_vec.z)));
+	return (tmp_normal);
+}
+
 t_insec_p	rt_cy_intersec(t_cylinder *cylinder, t_ray ray)
 {
 	double					t;
 	double 					flag;
-	t_insec_p	res;
+	double					h_dis;
+	t_insec_p				res;
+	t_3d_vec				pa;
 
 	flag = 1.0;
-	t = rt_cy_calculate_directional_vector_coefficients(cylinder, ray, &flag);
+	t = rt_cy_calc_dir_vec_t(cylinder, ray, &flag);
 	res.unit_n_vec.x = NOT_INTERSECT;
 	if (t <= 0)
-	{
 		return (res);
-	}
-
-	t_3d_vec pa = rt_get_point(ray, t);
-
-	double hDis = rt_vec_dot(rt_vec_sub(pa, cylinder->center_p_vec), cylinder->unit_orient_vec);
-	if ((- 1.0 * cylinder->height / 2.0f) <= hDis && hDis <= (cylinder->height / 2.0f))
+	pa = rt_get_point(ray, t);
+	h_dis = rt_vec_dot(rt_vec_sub(pa, cylinder->center_p_vec), cylinder->unit_orient_vec);
+	if ((- 1.0 * cylinder->height / 2.0f) <= h_dis && h_dis <= (cylinder->height / 2.0f))
 	{
 		res.dist = t * rt_vec_mag(ray.unit_d_vec);
 		res.p_vec = pa;
-		t_3d_vec	tmp_normal;
-		tmp_normal.x = 2 * flag * (cylinder->unit_orient_vec.z * (cylinder->unit_orient_vec.z * (pa.x - cylinder->center_p_vec.x) - cylinder->unit_orient_vec.x * (pa.z - cylinder->center_p_vec.z)) - \
-									cylinder->unit_orient_vec.y * (cylinder->unit_orient_vec.x * (pa.y - cylinder->center_p_vec.y) - cylinder->unit_orient_vec.y * (pa.x - cylinder->center_p_vec.x)));//法線方向の計算
-
-		tmp_normal.y = 2 * flag * (cylinder->unit_orient_vec.x * (cylinder->unit_orient_vec.x * (pa.y - cylinder->center_p_vec.y) - cylinder->unit_orient_vec.y * (pa.x - cylinder->center_p_vec.x)) - \
-									cylinder->unit_orient_vec.z * (cylinder->unit_orient_vec.y * (pa.z - cylinder->center_p_vec.z) - cylinder->unit_orient_vec.z * (pa.y - cylinder->center_p_vec.y)));//法線方向の計算
-
-		tmp_normal.z = 2 * flag * (cylinder->unit_orient_vec.y * (cylinder->unit_orient_vec.y * (pa.z - cylinder->center_p_vec.z) - cylinder->unit_orient_vec.z * (pa.y - cylinder->center_p_vec.y)) - \
-									cylinder->unit_orient_vec.x * (cylinder->unit_orient_vec.z * (pa.x - cylinder->center_p_vec.x) - cylinder->unit_orient_vec.x * (pa.z - cylinder->center_p_vec.z)));//法線方向の計算
-		res.unit_n_vec = rt_vec_to_unit(tmp_normal);
+		res.unit_n_vec = rt_vec_to_unit(rt_calc_normal(flag, cylinder, pa));
 		return (res);
 	}
 	return (res);
