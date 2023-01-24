c4ca84a01c361c3d2d88ecfcdc2131312a69513e
nfukuma <100755561+NaoyaFukuma@users.noreply.github.com>
2023-01-24 16:23:49 +0900

    norm fix

--- srcs/init/rt_init_hook.c
+++ srcs/init/rt_init_hook.c
@@ -6,7 +6,7 @@
 /*   By: nfukuma <nfukuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 13:08:13 by nfukuma           #+#    #+#             */
-/*   Updated: 2023/01/24 10:15:01 by nfukuma          ###   ########.fr       */
+/*   Updated: 2023/01/24 16:23:47 by nfukuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ static int	rt_destroynotify_hook(t_rt_data *rt);
 
 void	rt_hooks(t_rt_data *rt)
 {
-	mlx_hook(rt->mlx.win, KeyPress, 0, rt_keypress_hook, rt);
-	mlx_hook(rt->mlx.win, DestroyNotify, 0, rt_destroynotify_hook, rt);
+	mlx_hook(rt->mlx.win, KEYPRESS, 0, rt_keypress_hook, rt);
+	mlx_hook(rt->mlx.win, DESTROYNOTIFY, 0, rt_destroynotify_hook, rt);
 }
 
 static int	rt_keypress_hook(int keycode, t_rt_data *rt)
